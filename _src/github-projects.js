9ec4635ac6c0cd12bb13decddf2bd5b776fbdb27
CJ Davis <Christopher.Davis@cqlcorp.com>
2015-08-21 16:16:29 -0400

    Add reactjs and create placeholder for Git Hub project info

---
+++ _src/github-projects.js
@@ -0,0 +1,4 @@
+React.render(
+  <h2>GitHub Projects</h2>,
+  document.getElementById('github-projects')
+);
---
+++ js/github-projects.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement("h2", null, "GitHub Projects"),
+  document.getElementById('github-projects')
+);

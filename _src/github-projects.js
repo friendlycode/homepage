43502f090173dc19240e42cf5488e3d1b442783e
CJ Davis <cj@cjdavis.me>
2015-09-01 20:27:38 -0400

    Implement basic Github API fetching

--- _src/github-projects.js
+++ _src/github-projects.js
@@ -1,4 +1,41 @@
+var GithubRepositoryItem = React.createClass({
+  render: function() {
+    return (
+      <li>{this.props.repo.name}</li>
+    );
+  }
+});
+
+var GithubRepositoryList = React.createClass({
+  render: function() {
+    var repos = [];
+
+    jQuery.ajax({
+      url: this.props.source,
+      success: function(result) {
+                if(result.isOk == false) {
+                  alert(result.message);
+                } else {
+                  result.forEach(function(repo) {
+                    repos.push(<GithubRepositoryItem repo={repo} key={repo.name} />);
+                  });
+                }
+              },
+      async: false
+    }); 
+
+    return (
+      <div>
+        <h2>Github Projects</h2>
+        <ul>
+          {repos}
+        </ul>
+      </div>
+    );
+  }
+});
+
 React.render(
-  <h2>GitHub Projects</h2>,
+  <GithubRepositoryList source='https://api.github.com/orgs/friendlycode/repos' />,
   document.getElementById('github-projects')
 );
